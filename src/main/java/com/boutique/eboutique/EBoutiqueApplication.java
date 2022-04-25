633d02b0227bee41d4f8f3ff23ff4fea19ec6f67
Max <themax41@hotmail.fr>
2022-04-25 11:14:42 +0200

    Initialisation

---
+++ src/main/java/com/boutique/eboutique/EBoutiqueApplication.java
@@ -0,0 +1,13 @@
+package com.boutique.eboutique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBoutiqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBoutiqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boutique/eboutique/EBoutiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boutique.eboutique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBoutiqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
